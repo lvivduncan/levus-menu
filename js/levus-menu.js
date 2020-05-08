90272a7d0a8ce0ff4b2df657d1bd6b2548b450a8
lvivduncan <lvivduncan@gmail.com>
2020-05-08 19:03:39 +0300

    change .before to wrapper

--- js/levus-menu.js
+++ js/levus-menu.js
@@ -1,19 +1,26 @@
 // обгортка меню
 const menu = document.querySelector('#levus-menu');
 
+// обгортка для мобільного меню
+const wrapper = document.createElement('div');
+wrapper.className = 'levus-wrapper';
+
 // міняємо тип меню при зміні розміру вікна
 window.addEventListener('resize', changeMenu);
 
 // визначаємо при завантаженні сторінки
 document.addEventListener('DOMContentLoaded', changeMenu);
 
-menu.addEventListener('click', showMenu);
+wrapper.addEventListener('click', showMenu);
 
 // перемикаємо меню
 function changeMenu(){
   if(window.innerWidth < 1200){
     menu.classList.remove('desktop');
     menu.classList.add('mobile');
+
+    // додаємо елемент на сторінку
+    menu.append(wrapper);
   } else {
     menu.classList.remove('mobile');
     menu.classList.add('desktop');
