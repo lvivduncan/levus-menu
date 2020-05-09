6ec37827dd38673765cf8bcaaa1d251da7e34e78
lvivduncan <lvivduncan@gmail.com>
2020-05-09 18:46:25 +0300

    add use strict

--- js/levus-menu.js
+++ js/levus-menu.js
@@ -1,57 +1,61 @@
-// обгортка меню
-const menu = document.querySelector('#levus-menu');
+{
+  'use strict';
+  
+  // обгортка меню
+  const menu = document.querySelector('#levus-menu');
 
-// обгортка для мобільного меню
-const wrapper = document.createElement('div');
-wrapper.className = 'levus-wrapper';
+  // обгортка для мобільного меню
+  const wrapper = document.createElement('div');
+  wrapper.className = 'levus-wrapper';
 
-// усі li в меню
-const lis = menu.querySelectorAll('li');
+  // усі li в меню
+  const lis = menu.querySelectorAll('li');
 
-// додаємо клас для батьківського елемента
-lis.forEach(li => li.childNodes.length > 1 ? li.className = 'parent' : '');
+  // додаємо клас для батьківського елемента
+  lis.forEach(li => li.childNodes.length > 1 ? li.className = 'parent' : '');
 
-// міняємо тип меню при зміні розміру вікна
-window.addEventListener('resize', changeMenu);
+  // міняємо тип меню при зміні розміру вікна
+  window.addEventListener('resize', changeMenu);
 
-// визначаємо при завантаженні сторінки
-document.addEventListener('DOMContentLoaded', changeMenu);
+  // визначаємо при завантаженні сторінки
+  document.addEventListener('DOMContentLoaded', changeMenu);
 
-// розкрити меню на мобільному
-wrapper.addEventListener('click', showMenu);
+  // розкрити меню на мобільному
+  wrapper.addEventListener('click', showMenu);
 
-// розкриваємо/ховаємо вкладені списки
-lis.forEach(li => {
-  li.addEventListener('click', showUl);
-});
+  // розкриваємо/ховаємо вкладені списки
+  lis.forEach(li => {
+    li.addEventListener('click', showUl);
+  });
 
-// перемикаємо меню
-function changeMenu(){
-  if(window.innerWidth < 1200){
-    menu.classList.remove('desktop');
-    menu.classList.add('mobile');
+  // перемикаємо меню
+  function changeMenu() {
+    if (window.innerWidth < 1200) {
+      menu.classList.remove('desktop');
+      menu.classList.add('mobile');
 
-    // додаємо елемент на сторінку
-    menu.append(wrapper);
-  } else {
-    menu.classList.remove('mobile');
-    menu.classList.add('desktop');
-  }  
-}
-
-// показуємо/ховаємо мобільне меню
-function showMenu(){
-  if(menu.classList.contains('show')){
-    menu.classList.remove('show');
-  } else {
-    menu.classList.add('show');
+      // додаємо елемент на сторінку
+      menu.append(wrapper);
+    } else {
+      menu.classList.remove('mobile');
+      menu.classList.add('desktop');
+    }
   }
-}
 
-// показвти вкладені списки 
-function showUl(e){
-  e.stopPropagation();
-  if(e.target.classList.contains('parent')){
-    this.classList.toggle('show');
+  // показуємо/ховаємо мобільне меню
+  function showMenu() {
+    if (menu.classList.contains('show')) {
+      menu.classList.remove('show');
+    } else {
+      menu.classList.add('show');
+    }
+  }
+
+  // показвти вкладені списки 
+  function showUl(e) {
+    e.stopPropagation();
+    if (e.target.classList.contains('parent')) {
+      this.classList.toggle('show');
+    }
   }
 }
