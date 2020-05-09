9392c279abf01af7722b77e20fc789ca7c832996
lvivduncan <lvivduncan@gmail.com>
2020-05-09 14:20:56 +0300

    add parent

--- js/levus-menu.js
+++ js/levus-menu.js
@@ -5,6 +5,12 @@ const menu = document.querySelector('#levus-menu');
 const wrapper = document.createElement('div');
 wrapper.className = 'levus-wrapper';
 
+// усі li в меню
+const lis = menu.querySelectorAll('li');
+
+// додаємо клас для батьківського елемента
+lis.forEach(li => li.childNodes.length > 1 ? li.className = 'parent' : '');
+
 // міняємо тип меню при зміні розміру вікна
 window.addEventListener('resize', changeMenu);
 
